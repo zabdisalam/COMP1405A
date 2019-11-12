e5db86c9692db74ff3fd27e920a1d2cfcf764a7e
zabdisalam <kofigarebulasp@gmail.com>
2019-11-11 19:25:04 -0500

    working on fifth proj

--- Fall 2019/Assignment 5/comp1405_f19_zakaria_a5.py
+++ Fall 2019/Assignment 5/comp1405_f19_zakaria_a5.py
@@ -3,7 +3,7 @@ wordinputs = []
 fullinputstr = ''
 myinput = input("input ur char \n")
 
-def start():
+def main():
     global fullinput
     global wordinputs
     global fullinputstr
@@ -20,8 +20,8 @@ def start():
         print(f"{finalinput}", end='')
         fullinputstr = "".join(fullinput)
         wordinputs = fullinputstr.split()
-start()
-def main():
+main()
+def changeinput():
     global myinput
     changeivalues = input('\nWhat do you want change a phrase, word or letter\n')
     changedivalues = input(f"What {changeivalues} would you like to change?"
@@ -31,38 +31,5 @@ def main():
         changedivalues = wordinputs.index(changedivalues)
         wordinputs[changedivalues] = changeword
     myinput = " ".join(wordinputs)
-    start()
-main()
-
-# def cashregister(x):
-#     cents = [25, 10, 5, 1]
-#     for i in cents:
-#         if x-i >= 0:
-#             x = x-i
-#             print(i, end=' ')
-#             cashregister(x)
-#
-# cashregister(38)
-#
-# def change(n, coins_available, coins_so_far):
-#     if sum(coins_so_far) == n:
-#         yield coins_so_far
-#     elif sum(coins_so_far) > n:
-#         pass
-#     elif coins_available == []:
-#         pass
-#     else:
-#         for c in change(n, coins_available[:], coins_so_far+[coins_available[0]]):
-#             yield c
-#         for c in change(n, coins_available[1:], coins_so_far):
-#             yield c
-#
-# if __name__ == '__main__':
-#     n = 90
-#     coins = [1, 5, 10, 25]
-#
-#     solutions = [s for s in change(n, coins, [])]
-#     for s in solutions:
-#         s
-#
-#     print('optimal solution:', min(solutions, key=len))
+    main()
+changeinput()
